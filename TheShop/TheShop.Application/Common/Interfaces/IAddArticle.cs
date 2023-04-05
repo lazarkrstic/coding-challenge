87812e2374bc0b9d89b6baad1ea7fd4190a8c2d3
Lazar Krstic <lazarkrstic94@gmail.com>
2023-04-06 01:07:05 +0200

    Dodati interfejsi za Article inventory i Dealer-e

---
+++ TheShop/TheShop.Application/Common/Interfaces/IAddArticle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShop.Domain.Entities;
+
+namespace TheShop.Application.Common.Interfaces
+{
+    public interface IAddArticle
+    {
+        void SetArticle(Article article);
+    }
+}
