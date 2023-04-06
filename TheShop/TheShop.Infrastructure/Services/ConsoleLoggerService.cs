d4541129ab41360278447e110a2e54d052836832
Lazar Krstic <lazarkrstic94@gmail.com>
2023-04-06 23:47:33 +0200

    Dodati svi servisi osim Dealera

---
+++ TheShop/TheShop.Infrastructure/Services/ConsoleLoggerService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShop.Application.Common.Interfaces;
+
+namespace TheShop.Infrastructure.Services
+{
+    public enum LogLevel
+    {
+        Debug = 1,
+        Info = 2,
+        Error = 3,
+        None = 4
+    }
+    public class ConsoleLoggerService : ILogger
+    {
+        private readonly LogLevel _logLevel;
+
+        public ConsoleLoggerService(LogLevel logLevel = LogLevel.Debug)
+        {
+            _logLevel = logLevel;
+        }
+        public void LogDebug(string message)
+        {
+            LogToConsole(LogLevel.Debug, message);
+        }
+
+        public void LogError(string message)
+        {
+            LogToConsole(LogLevel.Error, message);
+        }
+
+        public void LogInfo(string message)
+        {
+            LogToConsole(LogLevel.Info, message);
+        }
+
+        private void LogToConsole(LogLevel level, string message)
+        {
+            if (level >= _logLevel)
+            {
+                Console.WriteLine($"{DateTime.Now}: {level} : {message}");   
+            }
+        }
+    }
+}
---
+++ TheShop/TheShop.Infrastructure/Services/WarehouseService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShop.Application.Common.Interfaces;
+using TheShop.Domain.Entities;
+
+namespace TheShop.Infrastructure.Services
+{
+    public class WarehouseService : IArticleInventory
+    {
+        public bool ArticleInInventory(uint id)
+        {
+            return new Random().NextDouble() >= 0.5;
+        }
+
+        public Article GetArticle(uint id)
+        {
+            return new Article()
+            {
+                Id = id,
+                Name = $"Article {id}",
+                Price = (uint) new Random().Next(100, 500)
+            };
+        }
+    }
+}
